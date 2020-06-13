a7b210cb1ffbd5165596fa848bf23c218a8eaa92
umang <iumangsharma001@gmail.com>
2020-06-13 16:59:51 +0530

    added border

--- app/src/main/java/com/example/shmbrowser/Activity/MyDownloadActivity.kt
+++ app/src/main/java/com/example/shmbrowser/Activity/MyDownloadActivity.kt
@@ -11,29 +11,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.shmbrowser.R
 
 class MyDownloadActivity: AppCompatActivity() {
-
-    private lateinit var imageButton5: ImageButton
-    private lateinit var imageButton6: ImageButton
-    private lateinit var imageButton7: ImageButton
-    private lateinit var imageButton8: ImageButton
-    private lateinit var recyclerDownloads: RecyclerView
-    private lateinit var layoutManager: RecyclerView.LayoutManager
-    private lateinit var scrollView: ScrollView
-    private lateinit var ll: LinearLayout
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_download)
-
-        imageButton5 = findViewById(R.id.imageButton5)
-        imageButton6 = findViewById(R.id.imageButton6)
-        imageButton7 = findViewById(R.id.imageButton7)
-        imageButton8 = findViewById(R.id.imageButton8)
-        recyclerDownloads = findViewById(R.id.recyclerDownloads)
-        layoutManager = LinearLayoutManager(this@MyDownloadActivity)
-        scrollView = findViewById(R.id.scrollView)
-        ll = findViewById(R.id.ll)
-
     }
 
     override fun onBackPressed() {
